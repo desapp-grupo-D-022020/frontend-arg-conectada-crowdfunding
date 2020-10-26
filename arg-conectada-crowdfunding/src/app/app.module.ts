b52b11a913f932646dd320a3cf7ee1039432791e
CristianMartin <c.knoxville8@gmail.com>
2020-10-25 22:58:45 -0300

    agrego page spa básica para la home
    Signed-off-by: CristianMartin <c.knoxville8@gmail.com>

---
+++ arg-conectada-crowdfunding/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
