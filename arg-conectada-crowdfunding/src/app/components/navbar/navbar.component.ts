5a21d427f71a07c5126d4166bc6afbfb038226ed
CristianMartin <c.knoxville8@gmail.com>
2020-10-27 16:00:22 -0300

    agregado services y componentes para el login de usuario
    Signed-off-by: CristianMartin <c.knoxville8@gmail.com>

--- arg-conectada-crowdfunding/src/app/components/navbar/navbar.component.ts
+++ arg-conectada-crowdfunding/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenService } from '../../services/token.service';
+import { Router } from '@angular/router';
 declare var $ : any;
 
 @Component({
@@ -8,13 +10,36 @@ declare var $ : any;
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  isLogin = false;
+  roles: string[];
+  authority: string;
 
-  ngOnInit(): void {
+  constructor(private tokenService: TokenService, private router: Router) { }
+
+  ngOnInit() {
+    if (this.tokenService.getToken()) {
+      this.isLogin = true;
+      this.roles = [];
+      this.roles = this.tokenService.getAuthorities();
+      this.roles.every(rol => {
+        if (rol === 'ROLE_ADMIN') {
+          this.authority = 'admin';
+          return false;
+        }
+        this.authority = 'user';
+        return true;
+      });
+    }
+  }
+
+  logOut(): void {
+    this.tokenService.logOut();
+    this.isLogin = false;
+    this.authority = '';
+    this.router.navigate(['home']);
   }
 
   collapse():void{
     $('.navbar-collapse').collapse('hide');
   }
-
 }
